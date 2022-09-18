64f73669082d4287588c66ce5bc74613fb9072a5
Ямковой Данил <danil1998.01.08@outlook.com>
2022-09-18 16:47:37 +0500

    Add scubox,mergeMechan

--- Assets/Scripts/Infastucture/CompsitionRoot.cs
+++ Assets/Scripts/Infastucture/CompsitionRoot.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using Danil;
+using Infastucture;
+using UnityEngine;
 
 public class CompsitionRoot : MonoBehaviour
 {
-    private IPlayerMove _playerMove;
-    private IMessageSystem _messageSystem;
+    [SerializeField] private NoteSystem _noteSystem;
+    [SerializeField] private Player _player;
+    private IPlayerMove _playerMove => _player;
+    private IMessageSystem _messageSystem => _noteSystem;
 
     private void Awake()
     {
@@ -20,7 +24,7 @@ public class CompsitionRoot : MonoBehaviour
     {
         _messageSystem.OnPlayerMoved(_playerMove.Position);
     }
-    
+
     private void OnCollected()
     {
         _playerMove.Freeze();
