a443829ef9cb2b69f32a8e338b07368f80999c59
Nevalayshka <47535881+HeBoJl9IlIka@users.noreply.github.com>
2022-09-18 15:15:05 +0400

    push

--- Assets/Scripts/Infastucture/CompsitionRoot.cs
+++ Assets/Scripts/Infastucture/CompsitionRoot.cs
@@ -1,19 +1,12 @@
-using Danil;
-using Infastucture;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CompsitionRoot : MonoBehaviour
 {
-    [SerializeField] private NoteSystem _noteSystem;
-    [SerializeField] private Player _playerObject;
-
     private IPlayerMove _playerMove;
-    private IMessageSystem _messageSystem => _noteSystem;
+    private IMessageSystem _messageSystem;
 
     private void Awake()
     {
-        _playerMove = _playerObject;
-
         Compose();
     }
 
@@ -26,9 +19,8 @@ public class CompsitionRoot : MonoBehaviour
     private void OnPlayerMove()
     {
         _messageSystem.OnPlayerMoved(_playerMove.Position);
-        print(1);
     }
-
+    
     private void OnCollected()
     {
         _playerMove.Freeze();
--- Assets/Scripts/Tree/ArrowRotation.cs
+++ Assets/Scripts/Tree/ArrowRotation.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ArrowRotation : MonoBehaviour
 {
-    [SerializeField] private Player _player;
+    //[SerializeField] private Player _player;
 
     private void OnEnable()
     {
